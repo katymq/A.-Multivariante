1e1343764de9115967887182b2834a5ac75b62cc
katymq <katymq19@gmail.com>
2016-01-26 16:48:03 -0500

    Actualizacion

--- server.R
+++ server.R
@@ -1,14 +1,27 @@
 library(shiny)
 library(readxl)
+library(qcr)
+library(ggplot2)
+library(ggdendro)
+library(cluster)
 
 
 shinyServer(function(input,output){
   
   data <- reactive({
-    file1 <- input$file
-    if(is.null(file1)){return()}
-    read.table(file=file1$datapath, sep=input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
-    
+    # file es df con: name, size, type, datapath (ruta archiv temp) del archio cargado
+    file <- input$data
+    if(is.null(file)) {return()}
+    assign("x", file,envir = .GlobalEnv)
+    if(file$type=="text/csv"){
+      read.table(file$datapath, header = TRUE, sep = ";", dec=",",
+                 colClasses = c("character", rep("numeric",2)))
+    }else{
+      # con read_xl no se reconoce el formato, le añado el formato .xlsx al nombre
+      file.rename(file$datapath,
+                  paste(file$datapath, ".xlsx", sep=""))
+      read_excel(paste(file$datapath, ".xlsx", sep=""),sheet = 1,na = "")
+    }
   })
   
   output$filedf <- renderUI({
@@ -25,16 +38,54 @@ shinyServer(function(input,output){
   output$suma <- renderPrint({
     if(is.null(data())){return ()}
     summary(data())
+    class(data())
+    names(data())
+    dd <- data.frame(data())
+    ddd<-dd[,2:5]
+    d<-matrix(0,nrow(ddd),ncol(ddd))
+    for(i in 1:ncol(ddd)){
+      for(j in 1:nrow(ddd)){
+        aui<-sub(",",".",ddd[j,i])
+        d[j,i]<-as.numeric(aui)
+      }
+    }
+    covarianza<-cov(d)
+    correlaciones<-cor(d)
+    #componentes principales
+    pca<- prcomp(d)
+    
+    
   })
+  output$Analisis <- renderPlot({
+    p<-plot(pca)   
+    print(p)
+    
+  })
+  
+  
   
   
   # the following renderUI is used to dynamically generate the tabsets when the file is loaded. Until the file is loaded, app will not show the tabset.
   output$tb <- renderUI({
     if(is.null(data()))
-      h5("Datos Generales de la Base",br(), tags$img(src='bases.png', heigth=1000, width=1000))
+      h5(p(align='center',"INICIO"),br(), p(align='justify',"INTRODUCCIÓN. La aplicación a presentar, nos
+                                            ayudará a poder realizar informes estadísticos de los talleres de
+                                            BemerCar. Lo que facilitará el estudio y elanálisis de la entidad
+                                            estudiada.
+                                            Además presentará varios análisis multivariantes, que  estará enfocado
+                                            atratar de pronosticar el número de automoviles que
+                                            realizanmantenimiento, después de cierto kilometraje. Para realizar
+                                            una campaña marketing sobre los grupos óptimos.
+                                            OBJETIVOS
+                                            Identificar las familias que presentan mayor frecuencia en retornar a
+                                            los talleres.
+                                            Analizar las variables y presentar un análisis descriptivo general de BemerCar.
+                                            Reducir la dimensión del número de  variables, las cuales explican de
+                                            mejor manera el modelo. Aplicando el método de componentes
+                                            principales.
+                                            "))
     else
       tabsetPanel(tabPanel("Antecedentes", tableOutput("filedf")),tabPanel("Data", tableOutput("table")),tabPanel("Summary", tableOutput("suma")),
-                  tabPanel("Análisis de componentes principales", tableOutput("filedf")))
+                  tabPanel("Análisis de componentes principales", tableOutput("filedf")), tabPanel("Graf", tableOutput("analisis")))
   })
 })
-  
--- ui.R
+++ ui.R
@@ -1,4 +1,9 @@
 library(shiny)
+library(readxl)
+library(qcr)
+library(ggplot2)
+library(ggdendro)
+library(cluster)
 
 shinyUI(fixedPage( 
   HTML("<body style=background:##FAFAFA> </body>"),
@@ -8,7 +13,7 @@ shinyUI(fixedPage(
            tags$img(src = "logo12.jpg", width = "200px", height = "90px",border="2")
     ),
     column(width=4,offset = 0,
-           HTML("<div align=left> <font color=SteelBlue face=Cambria size=6> Informes BemerCar. </font> </div>")
+           HTML("<div align=left> <font color=SteelBlue face=Arial size=6> Informes BemerCar </font> </div>")
     ),
     column(width=4,offset = 0,
            tags$img(src = "logo21.jpg", width = "210px", height = "90px",border="3") 
@@ -17,11 +22,12 @@ shinyUI(fixedPage(
   HTML("<hr color=SteelBlue noshade=noshade />"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("file","Cargar Archivo"), # fileinput() function is used to get the file upload contorl option
-      helpText("Tamaño máximo del archivo 5MB"),
+      fileInput(inputId="data", label="Cargar Archivo", multiple = FALSE, 
+                accept = c(".xls",".xlsx", ".csv")),
+      helpText("TamaÃ±o mÃ¡ximo del archivo 5MB"),
       tags$hr(),
       sliderInput("slide", 
-                  label = "Número de Variables:",
+                  label = "NÃºmero de Variables:",
                   min = 3, max = 7, value = 3)
     ),
     mainPanel(
@@ -30,6 +36,5 @@ shinyUI(fixedPage(
     )
     
   )
-      )
-  )
-
+)
+)
